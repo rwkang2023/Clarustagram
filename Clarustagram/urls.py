46bea8baf42b786cd89ed9292d1977dd534a765c
rwkang@naver.com <kangrowon@gmail.com>
2023-01-05 15:15:20 +0900

    :alien: update settings.py views.py urls.py

--- Clarustagram/urls.py
+++ Clarustagram/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from Clarustagram import settings
 from Clarustagram.views import Main
 
 urlpatterns = [
@@ -23,3 +25,4 @@ urlpatterns = [
     path('', Main.as_view()),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
